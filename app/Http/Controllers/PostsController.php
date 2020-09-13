da094fa832791f735ac942647a910bc2f7014e8c
Karishmakarkii <karkikarishma143@gmail.com>
2020-09-13 21:50:10 +0545

    authenticted user

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,8 +6,28 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
      {
         return view('posts/create');
+    }
+
+    public function store()
+     {
+       $data = request()->validate([
+           'caption' => 'required',      //validation rules in the laravel
+           'image' => 'required|image'   //we can add as many as validation rules to  by using pipe notation '|'
+       ])  ;  
+
+       //authenticated user
+       auth()->user()->posts()->create($data);
+    //    \App\Models\Post::create($data); 
+           dd(request()->all());
+
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
 Route::get('/profile/{user}', 'App\Http\Controllers\ProfilesController@index')->name('profile.show');
 Route::get('/p/create' ,'App\Http\Controllers\PostsController@create');
-
-Auth::routes();
+Route::post('/p' ,'App\Http\Controllers\PostsController@store');
 
 // Route::get('/home', function () {
 //     return view('home');
